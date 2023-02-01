bea38c1c86a80d748262dd07cf05cf28fea6fdc0
Arturo Laurenzi <arturo.laurenzi@iit.it>
2023-02-01 09:53:06 +0100

    build --no-cache, travis

--- base/build.bash
+++ base/build.bash
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
-docker build --pull --tag xbot2_focal_base:latest . -f $DIR/Dockerfile
-docker build --tag xbot2_focal_base_nvidia:latest . -f $DIR/Dockerfile-nvidia
+docker build --pull --no-cache --tag xbot2_focal_base:latest . -f $DIR/Dockerfile
+docker build --no-cache  --tag xbot2_focal_base_nvidia:latest . -f $DIR/Dockerfile-nvidia
