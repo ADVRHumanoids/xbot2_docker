b9cbaca428bdb4519602cbc2adbbf09fdb985671
Arturo Laurenzi <arturo.laurenzi@iit.it>
2024-10-17 17:01:16 +0200

    build and run scripts for noble

--- noble/run.bash
+++ noble/run.bash
@@ -4,4 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
-docker compose build --pull
+docker compose up -d --no-recreate
+nohup docker compose exec dev terminator &
