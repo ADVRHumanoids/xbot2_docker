d2d8c78037aca21f603ae4954b5a65bcd6820277
Arturo Laurenzi <arturo.laurenzi@gmail.com>
2025-07-29 11:42:43 +0200

    support conditional builds && tag name

--- jammy/push.sh
+++ jammy/push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+TAG_NAME=${GITHUB_REF_NAME:-latest}
 
-docker tag jammy-dev:latest hhcmhub/xbot2-jammy-dev:latest
-docker push hhcmhub/xbot2-jammy-dev:latest
+docker tag jammy-dev:latest hhcmhub/xbot2-jammy-dev:$TAG_NAME
+docker push hhcmhub/xbot2-jammy-dev:$TAG_NAME
--- noble/push.sh
+++ noble/push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+TAG_NAME=${GITHUB_REF_NAME:-latest}
 
-docker tag noble-dev:latest hhcmhub/xbot2-noble-dev:latest
-docker push hhcmhub/xbot2-noble-dev:latest
+docker tag noble-dev:latest hhcmhub/xbot2-noble-dev:$TAG_NAME
+docker push hhcmhub/xbot2-noble-dev:$TAG_NAME
