d0199ee301c8d3d0e354c99ccde4f4d6731ad30f
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-08-05 17:54:28 +0200

    [skip ci] fix push.sh to upload latest tag too

--- jammy/push.sh
+++ jammy/push.sh
@@ -2,4 +2,6 @@
 TAG_NAME=${GITHUB_REF_NAME:-latest}
 
 docker tag jammy-dev:latest hhcmhub/xbot2-jammy-dev:$TAG_NAME
+docker tag jammy-dev:latest hhcmhub/xbot2-jammy-dev:latest
+docker push hhcmhub/xbot2-jammy-dev:latest
 docker push hhcmhub/xbot2-jammy-dev:$TAG_NAME
--- noble/push.sh
+++ noble/push.sh
@@ -2,4 +2,6 @@
 TAG_NAME=${GITHUB_REF_NAME:-latest}
 
 docker tag noble-dev:latest hhcmhub/xbot2-noble-dev:$TAG_NAME
+docker tag noble-dev:latest hhcmhub/xbot2-noble-dev:latest
+docker push hhcmhub/xbot2-noble-dev:latest
 docker push hhcmhub/xbot2-noble-dev:$TAG_NAME
