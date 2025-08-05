810a946f834d020a519e58e54074f36eee12b80f
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-08-05 15:35:12 +0200

    push latest tag too (focal)

--- focal/push.sh
+++ focal/push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 TAG_NAME=${GITHUB_REF_NAME:-latest}
+docker tag focal-dev:latest hhcmhub/xbot2-focal-dev:latest
 docker tag focal-dev:latest hhcmhub/xbot2-focal-dev:$TAG_NAME
+docker push hhcmhub/xbot2-focal-dev:latest
 docker push hhcmhub/xbot2-focal-dev:$TAG_NAME
