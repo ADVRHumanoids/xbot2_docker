552cabcfd5a7d183ceec9194c869f0743dff013d
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-09-01 13:59:56 +0200

    rm build to save space

--- robot-template/_build/robot-noble-ros2/scripts/build-xeno.bash
+++ robot-template/_build/robot-noble-ros2/scripts/build-xeno.bash
@@ -4,4 +4,4 @@ cd xbot2_ws
 forest grow circulo9_xbot2_device -j8 -v -m xeno --clone-depth 1
 forest grow ecat_master -j8 -v -m xeno --clone-depth 1
 
-rm -rf src/xbot2 src/ec_xbot2_client src/circulo9_xbot2_device src/ecat_master
+rm -rf src/xbot2 src/ec_xbot2_client src/circulo9_xbot2_device src/ecat_master build
